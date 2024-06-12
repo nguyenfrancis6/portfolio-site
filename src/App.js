f2c309f2cfe9f95fc8d47ab9368eca215f6bec0c
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-06-12 16:12:53 -0700

    Routing + navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App font-sans">
-      Hello world
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/f-logo.jpg";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex max-w-7xl w-full mx-auto justify-between items-center h-20">
+      <Link to="/" className="flex items-center">
+        <img className="h-16 w-auto" src={logo} alt="logo" />
+        <p className="text-lg">Francis Nguyen</p>
+      </Link>
 
-export default Navbar
+      <ul className="flex items-center gap-5 text-lg">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
