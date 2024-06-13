ef67ccde65df394c7ab8f2efc666e07488d1591d
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-06-12 17:02:42 -0700

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-blue-900 text-white py-4">
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex items-center gap-3 text-2xl pr-2">
+          <a href="https://www.linkedin.com/in/francisnnguyen/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/nguyenfrancis6" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+        </div>
+        <div>
+          <ul className="flex items-center gap-5 text-lg pl-2">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-lg">©2024 Francis Nguyen</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,23 @@ import logo from "../assets/f-logo.jpg";
 
 const Navbar = () => {
   return (
-    <nav className="flex max-w-7xl w-full mx-auto justify-between items-center h-20">
-      <Link to="/" className="flex items-center">
-        <img className="h-16 w-auto" src={logo} alt="logo" />
-        <p className="text-lg">Francis Nguyen</p>
-      </Link>
+    <header>
+      <nav className="flex max-w-7xl w-full mx-auto justify-between items-center h-20">
+        <Link to="/" className="flex items-center">
+          <img className="h-16 w-auto" src={logo} alt="logo" />
+          <p className="text-2xl font-bold">Francis Nguyen</p>
+        </Link>
 
-      <ul className="flex items-center gap-5 text-lg">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className="flex items-center gap-5 text-lg">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
