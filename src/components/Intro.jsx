2b427b4eb09afd8feb2c8b9746eeb3023c01e391
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-06-12 21:26:14 -0700

    Project section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,11 +6,11 @@ import resume from "../assets/FrancisNguyen_resume.pdf";
 
 const Intro = () => {
   return (
-    <section className="flex min-h-96 justify-center max-w-7xl mx-12 xl:mx-auto my-14 gap-4 bg-blue-900 p-8 rounded-2xl items-center">
+    <section className="flex h-full justify-center max-w-7xl mx-12 xl:mx-auto my-14 gap-4 bg-blue-900 p-8 rounded-2xl items-center">
       <div className="h-full max-w-xs md:max-w-lg flex flex-col gap-3 w-full">
-        <h1 className="font-bold text-5xl text-white">Hi, I'm Francis</h1>
-        <h2 className="text-2xl text-gray-500">Software Developer</h2>
-        <p className="leading-7 text-white text-lg max-w-sm">
+        <h1 className="font-bold text-3xl md:text-5xl text-white">Hi, I'm Francis</h1>
+        <h2 className="text-lg md:text-2xl text-gray-400">Software Developer</h2>
+        <p className="leading-7 text-white text-md md:text-lg max-w-sm">
           I am a recent college graduate with a degree in Computer Science from
           Cal Poly Pomona with expertise in software and web development.
           <span className="hidden md:inline">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import instagram from '../assets/instagram.png'
+import pokemon from '../assets/pokemonapp.png'
+import nft from '../assets/nftsite.png'
 
 const Projects = () => {
+  const projectData = [
+    {
+      id: "1",
+      title: "Instagram Clone",
+      stack: "React, Firebase, ChakraUI",
+      thumbnail: instagram,
+    },
+    {
+      id: "2",
+      title: "Pokemon Web App",
+      stack: "React, HTML/CSS",
+      thumbnail: pokemon,
+    },
+    {
+      id: "3",
+      title: "NFT Marketplace",
+      stack: "React, HTML/CSS",
+      thumbnail: nft,
+    },
+  ];
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="max-w-6xl flex flex-col items-center mx-auto gap-12 my-12 px-12">
+      <h1 className="font-bold text-4xl text-center">My Projects</h1>
+        <ul className="flex flex-col gap-8">
+          {projectData.map((project) => (
+            <li key={project.id}>
+              <div className="text-center w-full flex items-center bg-gray-200 rounded">
+                <div className="w-1/2">
+                  <img className="rounded " src={project.thumbnail} alt="" />
+                </div>
+                <div className="w-1/2">
+                  <h1 className="font-bold text-sm sm:text-lg md:text-3xl">{project.title}</h1>
+                  <p className="hidden md:block text-lg">{project.stack}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
