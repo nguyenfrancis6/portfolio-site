3107b8da375f8fe53cb4aaf30a88c7bff9ec9617
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-06-13 16:45:57 -0700

    Update favicon + add padding to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,12 +15,12 @@ const Navbar = () => {
   return (
     <header>
       <nav className="flex max-w-7xl w-full mx-auto justify-between items-center h-20 dark:text-white">
-        <Link to="/" className="flex items-center">
+        <Link to="/" className="flex items-center pl-8 xl:pl-0">
           <img className="h-16 w-auto dark:invert" src={logo} alt="logo" />
           <p className="hidden sm:block text-2xl font-bold">Francis Nguyen</p>
         </Link>
 
-        <ul className="flex items-center gap-5 text-lg">
+        <ul className="flex items-center gap-5 text-lg pr-12 xl:pr-0">
           <li>
             <Link to="/" className="hover:text-blue-700 transition-all duration-200">Home</Link>
           </li>
